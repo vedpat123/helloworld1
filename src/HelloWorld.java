d90fb1533a4c8e7e699b4145c63ad6c225a437f1
Vedant Patil <vedant.pat1@gmail.com>
2021-11-11 19:59:33 -0500

    change from world to vedant

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Vedant!");
         System.out.println("This is fun.");
         System.out.println("Is It? Yes it is");
     }
