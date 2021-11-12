5fef6f3143e6025093403c72ab8fb25581387c72
Vedant Patil <vedant.pat1@gmail.com>
2021-11-11 19:40:10 -0500

    First Check-in

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("This is fun.");
+        System.out.println("Is It? Yes it is");
+    }
+}
